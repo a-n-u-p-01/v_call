39e4437d055c16bd69fd625dcec5774ee0eb5a34
Anupam <89017260+a-n-u-p-01@users.noreply.github.com>
2023-05-31 18:26:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ let init = async () => {
 
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true
+    audio: false
   });
 
 const videoTracks = localStream.getVideoTracks();
@@ -125,7 +125,7 @@ let createPeerConnection = async (MemberId) =>{
   if (!localStream) {
     localStream = await navigator.mediaDevices.getUserMedia({
       video: true,
-      audio: true
+      audio: false
     });
     document.getElementById("user-1").srcObject = videoStream;
   }

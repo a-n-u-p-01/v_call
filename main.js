ff2c4e83a27d635a13a60e944eb9e6d51632a045
anupam546 <anutarai.2001@gmail.com>
2023-05-31 15:29:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ let init = async () => {
 
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    
+    audio: true,
   });
 
 const videoTracks = localStream.getVideoTracks();
@@ -93,7 +93,7 @@ let createPeerConnection = async (MemberId) =>{
   if (!localStream) {
     localStream = await navigator.mediaDevices.getUserMedia({
       video: true,
-      audio: false,
+      audio: true,
     });
     document.getElementById("user-1").srcObject = videoStream;
   }

75a5364d2697a8d9d9b64a304fce7194667eae6d
anupam546 <anutarai.2001@gmail.com>
2023-06-01 03:47:20 +0530

    upadate

--- lobby.js
+++ lobby.js
@@ -3,18 +3,19 @@ let toggleButtons = document.querySelectorAll(".btn");
 toggleButtons.forEach((button) => {
   let previousColor = button.style.background; // Store the previous background color
   button.addEventListener("click", () => {
-    let controlElement = document.getElementById("control");
-    if (controlElement.style.display === "none") {
-      controlElement.style.display = "block";
-      controlElement.classList.add("control-transition"); // Add the transition class
+    if (document.getElementById("control").style.display === 'none') {
+        document.getElementById("control").style.display= 'flex';
+        document.getElementById("control").style.justifyContent = 'center';
+        document.getElementById("control").style.alignItems = 'center';
+       
     } else {
-      controlElement.classList.remove("control-transition"); // Remove the transition class
-      controlElement.style.display = "none";
+        document.getElementById("control").style.display= 'none';
     }
 
     setTimeout(() => {
-      controlElement.style.display = "none"; // Reset the display property
-      controlElement.classList.remove("control-transition"); // Remove the transition class
-    }, 3000); // Revert back after 3 seconds (adjust the time as needed)
+        document.getElementById("control").style.display= 'none';; // Reset the background color
+      }, 30000); // Revert back after 3 seconds (adjust the time as needed)
+    
   });
+
 });

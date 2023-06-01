b1ce95ed7c52ac7930a4bb0026c1c91c1509a143
anupam546 <anutarai.2001@gmail.com>
2023-06-01 10:18:10 +0530

    update lobby

--- lobby.js
+++ lobby.js
@@ -1,21 +1,39 @@
 
-let toggleButtons = document.querySelectorAll(".btn");
-toggleButtons.forEach((button) => {
-  let previousColor = button.style.background; // Store the previous background color
-  button.addEventListener("click", () => {
-    if (document.getElementById("control").style.display === 'none') {
-        document.getElementById("control").style.display= 'flex';
+// let toggleButtons = document.querySelectorAll(".btn");
+// toggleButtons.forEach((button) => {
+//   let previousColor = button.style.background; // Store the previous background color
+//   button.addEventListener("click", () => {
+//     if (document.getElementById("control").style.display === 'none') {
+//         document.getElementById("control").style.display= 'flex';
+//         document.getElementById("control").style.justifyContent = 'center';
+//         document.getElementById("control").style.alignItems = 'center';
+       
+//     } else {
+//         document.getElementById("control").style.display= 'none';
+//     }
+
+//     setTimeout(() => {
+//         document.getElementById("control").style.display= 'none';; // Reset the background color
+//       }, 30000); // Revert back after 3 seconds (adjust the time as needed)
+    
+//   });
+
+// });
+let input;
+let verify = document.getElementById("verification");
+const password = String.fromCharCode(49, 50, 51);
+verify.addEventListener("submit", (e) => {
+  e.preventDefault();
+  input = String(e.target.password.value);
+  
+  if (input === password) {
+    console.log(password);
+
+ document.getElementById("control").style.display= 'flex';
+ document.getElementById("enter").style.display= 'none';
         document.getElementById("control").style.justifyContent = 'center';
         document.getElementById("control").style.alignItems = 'center';
-       
-    } else {
-        document.getElementById("control").style.display= 'none';
-    }
 
-    setTimeout(() => {
-        document.getElementById("control").style.display= 'none';; // Reset the background color
-      }, 30000); // Revert back after 3 seconds (adjust the time as needed)
-    
-  });
+  }
 
 });

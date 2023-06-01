27981cfe2579f6211334b54fc8866f611994a0b2
anupam546 <anutarai.2001@gmail.com>
2023-06-01 11:21:34 +0530

    update

--- lobby.js
+++ lobby.js
@@ -19,21 +19,50 @@
 //   });
 
 // });
+
+// ---------------------js for verification------------
 let input;
 let verify = document.getElementById("verification");
 const password = String.fromCharCode(49, 50, 51);
-verify.addEventListener("submit", (e) => {
+verify.addEventListener("submit", async (e) => {
   e.preventDefault();
   input = String(e.target.password.value);
   
   if (input === password) {
     console.log(password);
 
- document.getElementById("control").style.display= 'flex';
  document.getElementById("enter").style.display= 'none';
-        document.getElementById("control").style.justifyContent = 'center';
-        document.getElementById("control").style.alignItems = 'center';
+ document.getElementById("control").style.display= 'flex';
+ document.getElementById("control").style.justifyContent = 'center';
+ document.getElementById("control").style.alignItems = 'center';
 
   }
-
 });
+  // ---------------------------js for join and create room-----------------
+
+let CreateRoom = document.querySelector("#in > div:first-child");
+CreateRoom.addEventListener('click', async () =>{
+  console.log("create room");
+  document.getElementById("circle").style.display = 'none';
+  document.getElementById("create-form").style.display = 'block';
+})
+let JoinRoom = document.querySelector("#in > div:last-child");
+JoinRoom.addEventListener('click', async () =>{
+  console.log("Join room");
+  document.getElementById("circle").style.display = 'none';
+  document.getElementById("join-form").style.display = 'block';
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+

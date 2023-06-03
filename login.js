b74da6b3a790ca95b7fe5646ca75ed1466171eed
anupam546 <anutarai.2001@gmail.com>
2023-06-03 08:04:18 +0530

    update

--- login.js
+++ login.js
@@ -9,6 +9,6 @@ let verify = document.getElementById("verification");
 verify.addEventListener("submit", async (e) => {
   e.preventDefault();
   input_pwd = String(e.target.password.value);
-  window.location = `/Main/main.html?input-pwd=${input_pwd}`;
+  window.location = `/Main/index.html?input-pwd=${input_pwd}`;
 
 });
